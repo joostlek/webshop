1d7f6f9f52d70a4339faece71d9e243a1939a0c5
joostlek <joostlek@outlook.com>
2019-02-15 14:01:38 +0100

    🚀 First commit!
    Signed-off-by: joostlek <joostlek@outlook.com>

---
+++ src/main/java/nl/hu/webshop/WebshopApplication.java
@@ -0,0 +1,14 @@
+package nl.hu.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopApplication.class, args);
+    }
+
+}
+
