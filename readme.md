02270c7c6f6eedcd60ef89b02f3358014fc47478
joostlek <joostlek@outlook.com>
2019-03-07 12:15:33 +0100

    Add readme with badges for Travis and sonarcloud
    Signed-off-by: joostlek <joostlek@outlook.com>

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Webshop
+
+[![Coverage](https://sonarcloud.io/api/project_badges/measure?project=joostlek_webshop&metric=coverage)](https://sonarcloud.io/dashboard?id=joostlek_webshop)
+[![Build Status](https://travis-ci.com/joostlek/webshop.svg?branch=master)](https://travis-ci.com/joostlek/webshop)
--- src/main/java/nl/hu/bracketboys/webshop/WebShopApplication.java
+++ src/main/java/nl/hu/bracketboys/webshop/WebShopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebshopApplication {
+public class WebShopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebshopApplication.class, args);
+        SpringApplication.run(WebShopApplication.class, args);
     }
 
 }
