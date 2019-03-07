8e5e075e80f9ead9855806d142c1a0eeb50a7cac
joostlek <joostlek@outlook.com>
2019-03-07 16:15:45 +0100

    Add ModelMapper bean
    Signed-off-by: joostlek <joostlek@outlook.com>

--- backend/src/main/java/nl/hu/bracketboys/webshop/backend/WebShopApplication.java
+++ backend/src/main/java/nl/hu/bracketboys/webshop/backend/WebShopApplication.java
@@ -1,10 +1,17 @@
 package nl.hu.bracketboys.webshop.backend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebShopApplication {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WebShopApplication.class, args);
