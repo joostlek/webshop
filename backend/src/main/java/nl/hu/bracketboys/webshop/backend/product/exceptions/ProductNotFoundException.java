a83bbef7387378128e6f9d718aacc4e2333c3e09
Derk van den Bergh <bamibal98@gmail.com>
2019-03-12 10:21:04 +0100

    Revert "Up"
    This reverts commit 764c5469

--- backend/src/main/java/nl/hu/bracketboys/webshop/backend/product/exceptions/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package nl.hu.bracketboys.webshop.backend.product.exceptions;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(Long productId) {
-        super(String.format("Product with id %d not found", productId));
-    }
-}
