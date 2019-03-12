c87af40c7233146e29d077b57dde87a23fb83e3e
SantinodenBrave <santinodenbrave@protonmail.com>
2019-03-12 11:27:36 +0100

    begin productpage

--- frontend/src/Product.js
+++ frontend/src/Product.js
@@ -1,40 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 class Product extends Component {
 
     prod = {
         ProductId: "HT-1000",
         Category: "Laptops",
-        MainCategory: "Computer Systems",
-        TaxTarifCode: "1",
-        SupplierName: "Very Best Screens",
-        WeightMeasure: 4.2,
-        WeightUnit: "KG",
         Description: "Notebook Basic 15 with 2,80 GHz quad core, 15\" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro",
         Name: "Notebook Basic 15",
         DateOfSale: "2017-03-26",
         ProductPicUrl: "test-resources/sap/ui/demokit/explored/img/HT-1000.jpg",
         Status: "Available",
         Quantity: 10,
-
     };
-
-    test = {
-        tags: [' test', 'test2']
-    }
 
     render() {
         // return (
             {/*<div>{this.test.tags.map(tag => <li>{tag}</li> )}</div>*/}
-        const haha = Object.entries(this.prod).map(([key,value])=>{
+        const productKey = Object.entries(this.prod).map(([key,value])=>{
             return (
-                <div>{key} : {value.toString()}</div>
+                <div>key</div>
             )
         })
 
-        return(Object.entries(haha)[1]);
+        const productValue = Object.entries(this.prod).map(([key,value])=>{
+            return (
+                <div>{value.toString()}</div>
+            )
+        })
+
+        return(
+            <div className="card" style={{width: 18 + 'rem'}}>
+                <img className="card-img-top" src="..." alt="Card image cap"></img>
+                    <div className="card-body">
+                        <h5 className="card-title">{productValue[3]}</h5>
+                        <p className="card-text">{productValue[2]}</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+            </div>
+        );
         // );
     }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
+import App from "./App";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Product from "./Product";
 
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Product/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
