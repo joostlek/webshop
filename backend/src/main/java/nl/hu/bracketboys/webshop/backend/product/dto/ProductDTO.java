9ef7a607cf13bea6af2688ed66b854127d334b15
Derk van den Bergh <bamibal98@gmail.com>
2019-03-12 10:18:23 +0100

    Up

---
+++ backend/src/main/java/nl/hu/bracketboys/webshop/backend/product/dto/ProductDTO.java
@@ -0,0 +1,18 @@
+package nl.hu.bracketboys.webshop.backend.product.dto;
+
+import java.util.Date;
+
+public class ProductDTO {
+
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private Double price;
+
+    private Date created;
+
+    private Date updated;
+}
