3d25f7ca8681573bf3ec55f6490f0c84078cc818
joostlek <joostlek@outlook.com>
2019-03-12 10:03:35 +0100

    Add AddressRepository
    Signed-off-by: joostlek <joostlek@outlook.com>

---
+++ backend/src/main/java/nl/hu/bracketboys/webshop/backend/address/AddressRepository.java
@@ -0,0 +1,6 @@
+package nl.hu.bracketboys.webshop.backend.address;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
