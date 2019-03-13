cb0fdbab090fd67abcfeaabe4119978e09b0bdb3
Lou <lourens.kos@student.hu.nl>
2019-03-13 01:03:57 +0100

    Json handler improved + bootstrap layout
    Simplified json data usage
    Added grid layout+navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import logo from "./assets/logo.png";
 import Product from "./Product";
-import 'bootstrap/dist/css/bootstrap.css';
-
+import Navbar from "./Navbar";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <Product></Product>
+        <div className="row">
+          <div className="col-2">
+            <img src={logo} />
+          </div>
+          <div className="col-10">
+            <Navbar />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-2" />
+          <div className="col-10">
+            <Product />
+          </div>
+        </div>
       </div>
     );
   }
--- frontend/src/Product.js
+++ frontend/src/Product.js
@@ -1,48 +1,57 @@
-import React, { Component } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import test from "./assets/test.svg";
 
 class Product extends Component {
-
-    prod = {
-        ProductId: "HT-1000",
-        Category: "Laptops",
-        Description: "Notebook Basic 15 with 2,80 GHz quad core, 15\" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro",
-        Name: "Notebook Basic 15",
-        DateOfSale: "2017-03-26",
-        ProductPicUrl: "test-resources/sap/ui/demokit/explored/img/HT-1000.jpg",
-        Status: "Available",
-        Quantity: 10,
-    };
-
-    render() {
-        // return (
-            {/*<div>{this.test.tags.map(tag => <li>{tag}</li> )}</div>*/}
-        const productKey = Object.entries(this.prod).map(([key,value])=>{
-            return (
-                <div>key</div>
-            )
-        })
-
-        const productValue = Object.entries(this.prod).map(([key,value])=>{
-            return (
-                <div>{value.toString()}</div>
-            )
-        })
-
-        return(
-            <div className="card" style={{width: 18 + 'rem'}}>
-                <img className="card-img-top" src="..." alt="Card image cap"></img>
-                    <div className="card-body">
-                        <h5 className="card-title">{productValue[3]}</h5>
-                        <p className="card-text">{productValue[2]}</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-            </div>
-        );
-        // );
+  //Testdata
+  prod = [
+    {
+      ProductId: "HT-1000",
+      Category: "Laptops",
+      Description:
+        'Notebook Basic 15 with 2,80 GHz quad core, 15" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro',
+      Name: "Notebook Basic 15",
+      DateOfSale: "2017-03-26",
+      ProductPicUrl: test,
+      Status: "Available",
+      Quantity: 10
+    },
+    {
+      ProductId: "HT-1000",
+      Category: "Laptops",
+      Description:
+        'Notebook Basic 15 with 2,80 GHz quad core, 15" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro',
+      Name: "Notebook Basic 3000",
+      DateOfSale: "2017-03-26",
+      ProductPicUrl: test,
+      Status: "Available",
+      Quantity: 10
     }
+  ];
+
+  render() {
+    return (
+      <div>
+        {this.prod.map(prod => (
+          <div className="card" style={{ width: 18 + "rem" }}>
+            <img
+              className="card-img-top"
+              src={prod.ProductPicUrl}
+              alt="Card image cap"
+            />
+            <div className="card-body">
+              <h5 className="card-title">{prod.Name}</h5>
+              <p className="card-text">{prod.Description}</p>
+              <a href="#" className="btn btn-primary">
+                Bekijk product
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Product;
