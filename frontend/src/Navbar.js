0a91002b6a0e37dc0434086ccdc9a1178266da38
Lou <lourens.kos@student.hu.nl>
2019-03-18 12:19:27 +0100

    Routing foundation

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import logo from "./assets/logo.png";
+import { NavLink } from "react-router-dom";
 
 export class Navbar extends Component {
   render() {
@@ -26,55 +27,16 @@ export class Navbar extends Component {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">
+              <NavLink className="nav-link" to="/">
                 Dashboard <span className="sr-only">(current)</span>
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Account
-              </a>
+              <NavLink className="nav-link" to="/product">
+                Products
+              </NavLink>
             </li>
-            {/* <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">
-                  Action
-                </a>
-                <a class="dropdown-item" href="#">
-                  Another action
-                </a>
-                <div class="dropdown-divider" />
-                <a class="dropdown-item" href="#">
-                  Something else here
-                </a>
-              </div>
-            </li> */}
           </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input
-              className="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button
-              className="btn btn-outline-success my-2 my-sm-0"
-              type="submit"
-            >
-              Search
-            </button>
-          </form>
         </div>
       </nav>
     );
