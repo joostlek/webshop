48323008742083401d9f51f332ec6bc6b65803e3
joostlek <joostlek@outlook.com>
2019-03-22 10:49:32 +0100

    Fix swagger
    Signed-off-by: joostlek <joostlek@outlook.com>

--- backend/src/main/java/nl/hu/bracketboys/webshop/backend/config/SwaggerConfig.java
+++ backend/src/main/java/nl/hu/bracketboys/webshop/backend/config/SwaggerConfig.java
@@ -8,12 +8,16 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .protocols(new HashSet<>(Arrays.asList("https", "https")))
                 .host("webshop.joostlek.dev")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("nl.hu.bracketboys.webshop.backend"))
