0f733784d1ffba83fe1f2d435589dc4cdb6dfb59
santinoHU <santinodenbrave@protonmail.com>
2019-03-22 10:41:03 +0100

    Productpage customized

--- frontend/src/Product.js
+++ frontend/src/Product.js
@@ -1,48 +1,74 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./App.css";
+import "./product.css"
 import "bootstrap/dist/css/bootstrap.css";
-import test from "./assets/test.svg";
+import productimage from "./assets/product.jpg";
 
 class Product extends Component {
-  //Testdata
-  prod = [
-    {
-      ProductId: "5",
-      Name: "Komkommer",
-      Category: "Groente",
-      Price: 1
-    },
-    {
-      ProductId: "6",
-      Name: "Chips",
-      Category: "Snack",
-      Price: 1.5
-    }
-  ];
 
-  render() {
-    return (
-      <div>
-        <h1>Producten</h1>
-        <div className="row">
-          {this.prod.map(prod => (
-            <div className="col-4">
-              <div className="card" style={{ width: 18 + "rem" }}>
-                <img className="card-img-top" src={test} alt="Card image cap" />
-                <div className="card-body">
-                  <h5 className="card-title">{prod.Name}</h5>
-                  <p className="card-text">{prod.Description}</p>
-                  <a href="#" className="btn btn-primary">
-                    Bekijk product
-                  </a>
+    //Testdata
+    prod = [
+        {
+            ProductId: "5",
+            Name: "Corsair GS600 600 Watt PSU",
+            Description: "The Corsair Gaming Series GS600 is the ideal price/performance choice for mid-spec gaming PC",
+            Category: "Groente",
+            Price: 1234.00
+        },
+    ];
+
+    saveProductToSession(prod) {
+        for (let i=-0; i < prod.length;i++) {
+
+        }
+
+
+        let session = window.sessionStorage;
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="container-fluid">
+                    <div className="content-wrapper">
+                        <div className="item-container">
+                            {this.prod.map(prod => (
+                                <div className="container">
+                                    <div className="col-md-6 product-page__container">
+                                        <div className="product">
+                                            <img id="item-display" src={productimage}/>
+                                        </div>
+
+                                        <div className="product-title">{prod.Name}</div>
+                                        <div className="product-desc">{prod.Description}</div>
+                                        <div className="col-md-3"></div>
+                                        <div className="product-rating"><i className="fa fa-star gold"></i> <i
+                                            className="fa fa-star gold"></i> <i className="fa fa-star gold"></i> <i
+                                            className="fa fa-star gold"></i> <i className="fa fa-star-o"></i></div>
+                                        <div className="product-price">$ {prod.Price}</div>
+
+                                        {/* Foutmelding genereren op basis van voorraad */}
+
+                                        <div className="product-stock">Op voorraad</div>
+                                        <div className="btn-group cart">
+                                            <button type="button" className="btn btn-success btn-cart">
+                                                Voeg toe aan winkelwagen
+                                            </button>
+                                        </div>
+                                        <div className="btn-group wishlist">
+                                            <button type="button" className="btn btn-danger">
+                                                Terug naar de categorie
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          ))}
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default Product;
