8c77764ab14dee92e95e83013ca68c40ece9a830
lourens <ngx_lourens@hotmail.com>
2019-03-23 13:33:31 +0100

    router improved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,43 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Product from "./Product";
-import ProductList from "./components/ProductList";
+import Dashboard from "./Dashboard";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
-import Cart from './components/Cart';
+import Cart from "./components/Cart";
+import Categorie from "./Categorie";
 
 class App extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            <Navbar/>
-                        </div>
-                    </div>
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <Navbar />
+            </div>
+          </div>
 
-                    <div className="row">
-                        <div className="col-2">
-                            <Sidebar/>
-                        </div>
-                        <div className="col-10">
-                            <Switch>
-                                <Route path="/" component={Product} exact/>
-                                <Route path="/product" component={ProductList}/>
-                                <Route path="/cart" component={Cart}/>
-                            </Switch>
-                        </div>
-                    </div>
-                </div>
-            </BrowserRouter>
-        );
-    }
+          <div className="row">
+            <div className="col-2">
+              <Sidebar />
+            </div>
+            <div className="col-10">
+              <Switch>
+                <Route path="/" component={Dashboard} exact />
+                <Route path="/product/:id" component={Product} />
+                <Route path="/cart" component={Cart} />
+                <Route path="/categorie/:id" component={Categorie} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -1,17 +1,35 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export class Sidebar extends Component {
   render() {
     return (
-      <div>
-        <ul className="list-group">
-          <li className="list-group-item active">Producten</li>
-          <li className="list-group-item">Aanbiedingen</li>
-          <li className="list-group-item">Categoriën</li>
-        </ul>
-      </div>
+      <Router>
+        <div>
+          <ul className="list-group">
+            <li className="list-group-item ">Categoriën</li>
+            <li className="list-group-item">
+              <Link to="/categorie">Fruit</Link>
+            </li>
+            <li className="list-group-item">
+              <Link to="/categorie">Groente</Link>
+            </li>
+            <li className="list-group-item">
+              <Link to="/categorie">Zuivel</Link>
+            </li>
+            <li className="list-group-item">
+              <Link to="/categorie">Vlees</Link>
+            </li>
+            <li className="list-group-item">
+              <Link to="/categorie">Frisdrank</Link>
+            </li>
+          </ul>
+        </div>
+      </Router>
     );
   }
 }
+
+const Child = ({ match }) => <div>{match.params.id}</div>;
 
 export default Sidebar;
