9bdad058bd8f2b67c78649cb21fad18b5d022f27
joostlek <joostlek@outlook.com>
2019-03-24 19:58:16 +0100

    Fix server-side authentication
    Signed-off-by: joostlek <joostlek@outlook.com>

---
+++ backend/src/main/java/nl/hu/bracketboys/webshop/backend/security/JwtTokenProvider.java
@@ -0,0 +1,86 @@
+package nl.hu.bracketboys.webshop.backend.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.crypto.SecretKey;
+import javax.management.relation.Role;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtTokenProvider {
+
+    private SecretKey secretKey;
+
+    @Value("${security.jwt.token.expire-length:3600000}")
+    private long validityInMilliseconds = 3600000; // 1h
+
+    @Qualifier("userService")
+    @Autowired
+    private UserDetailsService myUserDetails;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    }
+
+    public String createToken(String username, Collection<? extends GrantedAuthority> grantedAuthorities) {
+
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put("auth", grantedAuthorities);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = myUserDetails.loadUserByUsername(getUsername(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public String resolveToken(HttpServletRequest req) {
+        String bearerToken = req.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new CustomException("Expired or invalid JWT token", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
---
+++ backend/src/main/java/nl/hu/bracketboys/webshop/backend/security/auth/AuthService.java
@@ -0,0 +1,38 @@
+package nl.hu.bracketboys.webshop.backend.security.auth;
+
+import nl.hu.bracketboys.webshop.backend.security.CustomException;
+import nl.hu.bracketboys.webshop.backend.security.JwtTokenProvider;
+import nl.hu.bracketboys.webshop.backend.user.UserServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements AuthServiceInterface {
+
+    private final UserServiceInterface userService;
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AuthService(UserServiceInterface userService, JwtTokenProvider jwtTokenProvider, AuthenticationManager authenticationManager) {
+        this.userService = userService;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public String signin(String username, String password) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+            return jwtTokenProvider.createToken(username, userService.getUserByEmail(username).getGrantedAuthorities());
+        } catch (AuthenticationException e) {
+            throw new CustomException("Invalid username/password supplied", HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+    }
+}
